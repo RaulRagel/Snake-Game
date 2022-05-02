1f05a3200fb2b22afe904eaa59a6c1333f000688
raul.rs3006@gmail.com <raul.rs3006@gmail.com>
2022-05-02 11:41:02 +0200

    Mobile development

--- src/initializer.js
+++ src/initializer.js
@@ -1,18 +1,18 @@
-
+var gameContainer;
 var game;
 var snake;
 var apple;
 var board;
 
 //10 fps
-var gameSpeed = 100;
+var gameSpeed = 150;
 var cells = 20;
 
 window.onload = function(){
 
-    var gameContainer = document.getElementById("game-container");
+    gameContainer = document.getElementById("game-container");
 
-    board = new Board(gameContainer, cells); //default: 20 (cells), 500 (boardTam)
+    board = new Board(gameContainer, cells, 350); //default: 20 (cells), 500 (boardTam)
     
     apple = new Apple(board); //default: random (x), random (y)
     snake = new Snake(board, apple); //default: 0 (x), 0 (y)
@@ -20,13 +20,66 @@ window.onload = function(){
     game = new SnakeGame(board, snake, apple, gameSpeed); //default: 100 (gameSpeed)
 
 
-
     
-
+    document.querySelector(".logo").addEventListener("click", () => {
+      
+      game.togglePlayPause()
+    })
 
 }
 
 
 
 
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
 
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches
+}                                                     
+                                                                         
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX
+    yDown = firstTouch.clientY;                                      
+};                                                
+                                                                         
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+                                                                         
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            /* right swipe */ 
+            if(snake && snake.direction != 'right')
+            snake.direction = 'left' 
+        } else {
+            /* left swipe */
+            if(snake && snake.direction != 'left')
+            snake.direction = 'right' 
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            /* down swipe */ 
+            if(snake && snake.direction != 'down')
+            snake.direction = 'up' 
+        } else { 
+            /* up swipe */
+            if(snake && snake.direction != 'up')
+            snake.direction = 'down' 
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
